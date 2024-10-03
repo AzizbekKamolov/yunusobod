43ad3e40711189043c1dec0b854bdfc757ffec07
AzizbekKamolov <azizbekkamolov19@gmail.com>
2024-10-03 08:24:35 +0500

    sdf

--- app/Models/RequestModel.php
+++ app/Models/RequestModel.php
@@ -2,10 +2,35 @@
 
 namespace App\Models;
 
+use App\Filters\Trait\EloquentFilterTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id;
+ * @property string $fio;
+ * @property string $email;
+ * @property string $phone;
+ * @property string $title;
+ * @property string $content;
+ * @property int $status;
+ * @property string $created_at;
+ * @property string $updated_at;
+ */
 class RequestModel extends Model
 {
-    use HasFactory;
+    use HasFactory, EloquentFilterTrait;
+
+    protected $table = 'requests';
+    protected $fillable = [
+        "id",
+        "fio",
+        "email",
+        "phone",
+        "title",
+        "content",
+        "status",
+        "created_at",
+        "updated_at",
+    ];
 }
--- app/Models/SettingModel.php
+++ app/Models/SettingModel.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
+use App\Filters\Trait\EloquentFilterTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id;
+ * @property string $name;
+ * @property array $value;
+ * @property string $created_at;
+ * @property string $updated_at;
+ *
+ */
 class SettingModel extends Model
 {
-    use HasFactory;
+    use HasFactory, EloquentFilterTrait;
+
+    protected $table = 'settings';
+    protected $fillable = [
+        'id',
+        'name',
+        'value',
+        'created_at',
+        'updated_at',
+    ];
 }
